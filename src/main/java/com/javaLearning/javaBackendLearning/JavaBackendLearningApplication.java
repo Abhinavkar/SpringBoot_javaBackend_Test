17311d45b8c36a8b5666dc2c06bec2b3131c83a8
Abhinav kar <karabhinav24@gmail.com>
2024-06-24 19:50:55 +0530

    Initialization of Spring Boot

---
+++ src/main/java/com/javaLearning/javaBackendLearning/JavaBackendLearningApplication.java
@@ -0,0 +1,13 @@
+package com.javaLearning.javaBackendLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaLearning/javaBackendLearning/JavaBackendLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaLearning.javaBackendLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
